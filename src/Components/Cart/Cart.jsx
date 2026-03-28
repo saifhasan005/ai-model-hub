018a378be13c4bac809766b7874332ece3229414
Saif205-svg <rafiuzzamansaif005@gmail.com>
2026-03-28 22:27:00 +0600

    cart section added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -6,7 +6,7 @@ const Cart = ({ selectedData }) => {
         <div className='max-w-[1300px] mx-auto'>
             <h1 className='text-3xl font-bold '>Your Carts</h1>
             {
-                selectedData.map(data => <div className=' items-center flex p-4 rounded-xl mb-[20px] border-zinc-300 justify-between border mt-[20px]' key={data.id}>
+                selectedData.map(data => <div className=' items-center flex p-4 rounded-xl mb-[20px] bg-zinc-300 border-zinc-300 justify-between border mt-[20px]' key={data.id}>
                     <div className='flex justify-center items-center gap-2 mt-[15px]'>
                         <div>
                             <img className='h-20 w-20 object-contain' src={data.image} alt="" />
@@ -14,6 +14,7 @@ const Cart = ({ selectedData }) => {
 
                         <div>
                             <h2 className='font-bold text-2xl'>{data.title}</h2>
+                            <p>{data.description}</p>
                         </div>
 
                     </div>
@@ -23,6 +24,11 @@ const Cart = ({ selectedData }) => {
 
                 </div>)
             }
+            <div className='flex justify-between mb-[15px] p-[12px] bg-red-400'>
+                <div className='font-bold text-2xl'>Total</div>
+                <div>0</div>
+            </div>
+
         </div>
     );
 };
